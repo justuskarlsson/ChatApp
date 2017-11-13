29a1fa4b62675cb6e1c6efc518529ff7544919a3
jullinator <justus.karlsson@hotmail.se>
2017-11-13 17:09:23 +0100

    socket Check

--- socket.js
+++ socket.js
@@ -14,25 +14,33 @@ class Store {
         userInfo: {0:0},
         rooms: {0:0},
     }
-    update = (key, val, route = "", args = {}) => {
+    update = (key, val, route = "", data = {}) => {
         this[key] = val
         // maybe add check so that 
         if (this._listeners[key]){
-            Object.values(this._listeners[key]).map( lFunc =>{
+            Object.entries(this._listeners[key]).map( ([key,{func, check}]) =>{
+                if(key == 0){
+                    continue
+                }
                 // if lCheck => lCheck(route, args)
-                lFunc(val)
+                if( check(route, data) ){
+                    func(val)
+                }
             })
         }
 
     }
 
-    on = (key, func) => {
+    on = (key, func, check=(route, data)=>(true) ) => {
         // function to check if update is valied
         // (route, args) => route == "message/new" && args.roomID == this.state.id
         this._listeners[key][0] ++
         var id = this._listeners[key][0]
 
-        this._listeners[key][id] = lFunc
+        this._listeners[key][id] = {
+            func,
+            check
+        }
         console.log(`Listener ${id} on ${key} was added.`)
         const remove = ()=> {
             delete this._listeners[key][id]
@@ -57,7 +65,7 @@ socket.onmessage = (e) =>{
             store.rooms.map(room => room.id == data.roomID ? 
                 {...room, messages:[ ...room.messages, {id, content, fromUserID, dateSent}]}
                 : room
-            )
+            ), route, data
         )
     }
     else if(route == "init") {
